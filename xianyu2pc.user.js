fc67ac08f767a384843a392e0f286f2f5857df34
lihaoyun6 <lihaoyun11@gmail.com>
2023-04-25 13:46:58 +0800

    Update xianyu2pc.user.js

--- xianyu2pc.user.js
+++ xianyu2pc.user.js
@@ -1,24 +1,25 @@
 // ==UserScript==
 // @name         闲鱼强制PC版页面
 // @namespace    https://github.com/lihaoyun6/xianyu2pc
-// @version      0.3
-// @description  访问到移动版咸鱼页面时自动转到对应的PC版页面
+// @version      0.3.2
+// @description  访问到移动版闲鱼页面时自动转到对应的PC版页面
 // @author       lihaoyun6
-// @include      *//2.famecl.com*.detail.*
-// @include      *//g.alicdn.com*.detail.*
+// @license      MIT
+// @match        *://h5.m.goofish.com/*
 // @run-at     	 document-start
 // @grant        none
+// @icon         https://img.alicdn.com/tps/i3/TB1eW1eGXXXXXXAXFXXBS8UGFXX-41-22.png
 // ==/UserScript==
 let xianyuMobileObj = {
 	init () {
 		let curr_href = location.href;
-        let cat_arr = /\.detail\.(\d+)\.?/.exec(curr_href);
+        let cat_arr = /id=(\d+)&/.exec(curr_href);
         if (cat_arr == null) {
 			console.log('log: detail info id not found.');
-			die();
+			return;
 		}
 		let id = cat_arr[1];
-		location.href = 'https://2.taobao.com/item.htm?id=' + id; // done !
+		location.href = 'https://item.taobao.com/item.htm?id=' + id; // done !
 	}
 };
 
